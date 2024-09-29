32739c2e83d6cc79db19b4f783ae6a059190dedd
Garrett Morse <garrett.morse@jpmchase.com>
2024-09-29 17:38:01 -0400

    feat: add bubble and mini fonts

--- scripts/testy.cjs
+++ scripts/testy.cjs
@@ -1,12 +1,16 @@
 const { FIGlet } = require("../dist/cjs/index.cjs");
 const fs = require("node:fs");
 
-const esmFonts = fs.readdirSync(process.cwd() + "/dist/cjs/fonts");
+const cjsFonts = fs.readdirSync(process.cwd() + "/dist/cjs/fonts");
 
-const testStr = "Hello, World"
+const testStr = "!.Hello, World"
 
-esmFonts.filter(file => file.slice(-4) === '.cjs').forEach(font => {
+console.log('***\nCJS\n***')
+cjsFonts.filter(file => file.slice(-4) === '.cjs').forEach(font => {
     const config = require(process.cwd() + `/dist/cjs/fonts/${font}`);
     const fonty = new FIGlet(config[font.slice(0, -4)]);
-    console.log(fonty.write(testStr));
+    const fontNameLen = font.slice(0,-4).length;
+    console.log(`---${font.slice(0,-4)}---`);
+    process.stdout.write(fonty.write(testStr));
+    console.log(`---${"=".repeat(fontNameLen)}---`);
 });
--- scripts/testy.js
+++ scripts/testy.js
@@ -5,8 +5,12 @@ const esmFonts = fs.readdirSync(process.cwd() + "/dist/fonts");
 
 const testStr = "Hello, World"
 
+console.log('***\nESM\n***')
 esmFonts.filter(file => file.slice(-3) === '.js').forEach(async font => {
     const config = await import(process.cwd() + `/dist/fonts/${font}`);
     const fonty = new FIGlet(config[font.slice(0, -3)]);
-    console.log(fonty.write(testStr));
+    const fontNameLen = font.slice(0,-3).length;
+    console.log(`---${font.slice(0,-3)}---`);
+    process.stdout.write(fonty.write(testStr));
+    console.log(`---${"=".repeat(fontNameLen)}---`);
 });
