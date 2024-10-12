e9478e73a092166aa901370c05bb342a67bc5fa9
Garrett Morse <garrett.morse@jpmchase.com>
2024-10-12 14:02:45 -0400

    chore: update tests

--- scripts/testy.cjs
+++ scripts/testy.cjs
@@ -3,10 +3,12 @@ const fs = require("node:fs");
 
 const cjsFonts = fs.readdirSync(process.cwd() + "/dist/cjs/fonts");
 
-const testStr = "!.Hello, World"
+const testStr = "Hello, World"
+
+const font = process.argv.slice(2)?.at(0);
 
 console.log('***\nCJS\n***')
-cjsFonts.filter(file => file.slice(-4) === '.cjs').forEach(font => {
+cjsFonts.filter(file => (font ? font === file.slice(0, -4) : true) && file.slice(-4) === '.cjs').forEach(font => {
     const config = require(process.cwd() + `/dist/cjs/fonts/${font}`);
     const fonty = new FIGlet(config[font.slice(0, -4)]);
     const fontNameLen = font.slice(0,-4).length;
--- scripts/testy.js
+++ scripts/testy.js
@@ -5,8 +5,10 @@ const esmFonts = fs.readdirSync(process.cwd() + "/dist/fonts");
 
 const testStr = "Hello, World"
 
+const font = process.argv.slice(2)?.at(0);
+
 console.log('***\nESM\n***')
-esmFonts.filter(file => file.slice(-3) === '.js').forEach(async font => {
+esmFonts.filter(file => (font ? font === file.slice(0, -3) : true) && file.slice(-3) === '.js').forEach(async font => {
     const config = await import(process.cwd() + `/dist/fonts/${font}`);
     const fonty = new FIGlet(config[font.slice(0, -3)]);
     const fontNameLen = font.slice(0,-3).length;
